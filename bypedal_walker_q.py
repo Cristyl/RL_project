11d9775a98d997c99683615648f1b4690c6e8590
Cristyl <cristianmm1240@gmail.com>
2024-07-22 09:27:15 +0200

    version 0.2
    Must:
    incremental timestamps of episodes
    debugging
    optimization
    q-learning for continous states

--- bypedal_walker_q.py
+++ bypedal_walker_q.py
@@ -1,12 +1,11 @@
 import gymnasium as gym
 import random
 from gymnasium.wrappers import TimeLimit
-env = gym.make("BipedalWalker-v3", render_mode = "human")
+env = gym.make("BipedalWalker-v3")
 max_steps = 1600  # Specify the maximum number of steps
 env = TimeLimit(env, max_episode_steps=max_steps)
 observation, info = env.reset()
 look_up_table = {}
-alpha = 0.5
 gamma = 0.9
 epsylon = 0.3
 
@@ -15,15 +14,16 @@ for i in range(100000):
     #choose action a (chooses randamly an action)
     random_num = random.uniform(0,1)
     r = 0
+    visits = 0
     action = None
     observation = tuple(observation)
     if random_num >= epsylon:
         #choose best action so far
         for elem in look_up_table:
             if observation == elem[0]:
-                if look_up_table[elem] > r:
+                if look_up_table[elem][0] > r:
                     action = elem[1]
-                    r = look_up_table[elem]
+                    r = look_up_table[elem][0]
     if random_num < epsylon or action is None:
         action = env.action_space.sample()
     action = tuple(action)
@@ -34,22 +34,24 @@ for i in range(100000):
     observation1 = tuple(observation1)
     #update or insert in table Q[s,a] as (1 - alpha)Q[s,a] + alpha(r + gamma X max(a')Q[s',a'])
     #define max_q as gretest reward for s'
-    max_q = 0
-    elements = []
+    max_q = float('-inf')
     for elem in look_up_table:
-        if (observation1, action) == elem:
-            max_q = max(max_q, look_up_table[elem])
+        if observation1 == elem[0]:
+            max_q = max(max_q, look_up_table[elem][0])
+    visits = look_up_table.get((observation, action), (0, 0))[1]
+    alpha = 1 / (1 + visits)
     reward = (1 - alpha) * reward + alpha * (reward + gamma * (max_q))
     if (observation, action) not in look_up_table:
-        look_up_table[(observation, action)] = reward
+        look_up_table[(observation, action)] = (reward, 0)
     else:
         #modify reward
         #modify reward in look_up_table
-        look_up_table.update({(observation, action): reward})
+        current_reward, current_counter = look_up_table[(observation, action)]
+        new_reward = reward
+        new_counter = current_counter + 1
+        look_up_table[(observation, action)] = (new_reward, new_counter)
+    observation = observation1
     if terminated or truncated:
         observation, info = env.reset()
-    if reward == 300:
-        print("found policy")
-        env.close()
 print("look_up_table_len: ", len(look_up_table))
 env.close()
